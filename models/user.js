fe797a4a6c87cdf9d515b737bf483b2eacdc1b7b
bogdanberdnikov <bogdan.b@legalbet.com>
2019-07-09 15:56:07 +0600

    Add password hashing

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: {
@@ -20,12 +23,45 @@ module.exports = (sequelize, DataTypes) => {
     },
     hash: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      set(password) {
+        this.setDataValue('hash', this.encodePass(password));
+      }
     }
   });
 
   User.associate = function (models) {
     // associations can be defined here
+  };
+
+  User.prototype.encodePass = function (password) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    return crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
+  };
+
+  User.prototype.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+  };
+
+  User.prototype.generateJWT = function() {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+      email: this.email,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, 'secret');
+  };
+
+  User.prototype.toAuthJSON = function() {
+    return {
+      _id: this._id,
+      email: this.email,
+      token: this.generateJWT(),
+    };
   };
 
   return User;
